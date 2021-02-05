28ee07ac8c77ee9a9bbfe55b3f820a03c430016f
Dave Bullis <dave-sf@sillub.com>
2021-02-05 11:41:58 -0500

    Remove cnvdsgn's dependency on messages.h

--- app/bin/cnvdsgn.c
+++ app/bin/cnvdsgn.c
@@ -16,7 +16,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#include "common.h"
+#include "utility.h"
 
 #define GETMAXY \
 		if (lp->y0 > maxY) maxY = lp->y0; \
