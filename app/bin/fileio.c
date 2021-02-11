9a48ea212926a56b2056db8d03d1ea87a884be47
Russell Shilling <rshilling@users.sourceforge.net>
2021-02-11 13:12:37 -0800

    commit recentfile

--- app/bin/fileio.c
+++ app/bin/fileio.c
@@ -810,9 +810,6 @@ int LoadTracks(
 	if (access(fileName[0], R_OK) != 0)
 	{
 		NoticeMessage(MSG_OPEN_FAIL, _("Continue"), NULL, _("Track"), nameOfFile, _("Not Found"));
-		if (NoticeMessage(_("Remove entry for %s?"), _("Yes"), _("No"), nameOfFile) > 0) {
-			wMenuListDelete(fileList_ml, nameOfFile);
-		}
 		return FALSE;
 	}
 
