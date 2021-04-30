520e80f8c69ea86cc22e910b0932e0955bcf0785
Dave Bullis <dave-sf@sillub.com>
2021-04-30 14:35:12 -0400

    one more

--- app/bin/draw.h
+++ app/bin/draw.h
@@ -241,11 +241,11 @@ void DrawRuler(drawCmd_p, coOrd, coOrd, DIST_T, int, int, wDrawColor);
 void MainProc(wWin_p, winProcEvent, void *, void *);
 void InitInfoBar(void);
 void DrawInit(int);
-void DoZoomUp(void * param);
-void DoZoomDown(void * param);
-void DoZoomExtents( void * param);
-void PanHere(void * param);
-void PanMenuEnter(void * param);
+void DoZoomUp(void * modeVP);
+void DoZoomDown(void * modeVP);
+void DoZoomExtents( void * modeVP);
+void PanHere(void * modeVP);
+void PanMenuEnter(void * modeVP);
 
 void InitCmdZoom(wMenu_p, wMenu_p, wMenu_p, wMenu_p);
 
