e4dbfc1046347a2d0b6d1b3ab22fbc1adc35329d
Martin Fischer <m_fischer@users.sourceforge.net>
2021-06-02 18:19:22 +0200

    Add dialog for Layout Control Elements and Signals

--- app/bin/csignal.c
+++ app/bin/csignal.c
@@ -522,7 +522,7 @@ static paramData_t signalEditPLs[] = {
 #define I_SIGNALASPECTDELETE (8)
     /*8*/ { PD_BUTTON, (void*)AspectDelete, "delete", 0, NULL, N_("Delete Aspect") },
 };
-static paramGroup_t signalEditPG = { "signalEdit", F_USETEMPLATE, signalEditPLs, sizeof signalEditPLs/sizeof signalEditPLs[0] };
+static paramGroup_t signalEditPG = { "signalEdit", PGO_DIALOGTEMPLATE, signalEditPLs, sizeof signalEditPLs/sizeof signalEditPLs[0] };
 static wWin_p signalEditW;
 
 static paramIntegerRange_t rm1_999999 = { -1, 999999 };
@@ -536,7 +536,7 @@ static paramData_t aspectEditPLs[] = {
     /*2*/ { PD_LONG,   &signalAspectEditIndex, "index", PDO_NOPREF, &rm1_999999, N_("Aspect Index"), BO_READONLY },
 };
 
-static paramGroup_t aspectEditPG = { "aspectEdit", F_USETEMPLATE, aspectEditPLs, sizeof aspectEditPLs/sizeof aspectEditPLs[0] };
+static paramGroup_t aspectEditPG = { "aspectEdit", PGO_DIALOGTEMPLATE, aspectEditPLs, sizeof aspectEditPLs/sizeof aspectEditPLs[0] };
 static wWin_p aspectEditW;
 
 
