80a831fe655c75de0334fda70dc123751fdaba6b
Martin Fischer <m_fischer@users.sourceforge.net>
2021-08-17 18:50:17 +0200

    Add copyright information for software distributed wioth XTrackCAD

--- app/bin/smalldlg.c
+++ app/bin/smalldlg.c
@@ -205,19 +205,29 @@ void CreateAboutW(void *ptr)
 		aboutW = ParamCreateDialog(&aboutPG, MakeWindowTitle(_("About")), NULL, NULL, wHide, FALSE, NULL, F_TOP | F_CENTER| PD_F_ALT_CANCELLABEL, NULL);
 		ParamLoadMessage(&aboutPG, I_ABOUTVERSION, sAboutProd);
 		wTextAppend(COPYRIGHT_T, DESCRIPTION);
-		wTextAppend(COPYRIGHT_T, "\n\nXTrackCAD is Copyright 2003 by Sillub Technology and 2017 by Bob Blackwell, Martin Fischer and  Adam Richards.");
-		wTextAppend(COPYRIGHT_T, "\nIcons by: Tango Desktop Project (http://tango.freedesktop.org)");
-		wTextAppend(COPYRIGHT_T, "\nSome icons by Yusuke Kamiyamane. Licensed under a Creative Commons Attribution 3.0 License.");
-		wTextAppend(COPYRIGHT_T, "\nContributions by: Robert Heller, Mikko Nissinen, Timothy M. Shead, Daniel Luis Spagnol");
-		wTextAppend(COPYRIGHT_T, "\nParameter Files by: Ralph Boyd, Dwayne Ward");
+		wTextAppend(COPYRIGHT_T, "\n\nXTrackCAD is Copyright 2003 by Sillub Technology and 2017 by Bob Blackwell, Martin Fischer and  Adam Richards.\n");
+		wTextAppend(COPYRIGHT_T, "\nIcons by: Tango Desktop Project (http://tango.freedesktop.org)\n");
+		wTextAppend(COPYRIGHT_T, "\nSome icons by Yusuke Kamiyamane. Licensed under a Creative Commons Attribution 3.0 License.\n");
+		wTextAppend(COPYRIGHT_T, "\nContributions by: Robert Heller, Mikko Nissinen, Timothy M. Shead, Russell Shilling, Daniel Luis Spagnol");
+		wTextAppend(COPYRIGHT_T, "\nParameter Files by: Ralph Boyd, Dwayne Ward\n");
+
+		wTextAppend(COPYRIGHT_T, "\nThe following software is distributed with XTrackCAD\n\n");
 #ifdef WINDOWS
-		wTextAppend(COPYRIGHT_T, "\n");
 		wTextAppend(COPYRIGHT_T, FreeImage_GetCopyrightMessage());
+		wTextAppend(COPYRIGHT_T, "\n\n");
 #endif
-		wTextAppend(COPYRIGHT_T, "\nCornu Algorithm and Implementation by: Raph Levien");
-		wTextAppend(COPYRIGHT_T, "\nuthash, utlist Copyright notice:");
+		wTextAppend(COPYRIGHT_T, "Cornu Algorithm and Implementation by: Raph Levien");
+		wTextAppend(COPYRIGHT_T, "\n\nuthash, utlist Copyright notice:");
 		wTextAppend(COPYRIGHT_T, "\nCopyright (c) 2005-2015, Troy D. Hanson  http://troydhanson.github.com/uthash/");
 		wTextAppend(COPYRIGHT_T, "\nAll rights reserved.");
+
+		wTextAppend(COPYRIGHT_T, "\n\ncJSON: Copyright (c) 2009-2017 Dave Gamble and cJSON contributors");
+		wTextAppend(COPYRIGHT_T, "\n\nlibzip:  Copyright(C) 1999 - 2019 Dieter Baron and Thomas Klausner\n" \
+								  "The authors can be contacted at libzip@nih.at");
+
+		wTextAppend(COPYRIGHT_T, "\n\nMiniXML: Copyright © 2003-2019 by Michael R Sweet.\n" \
+			"The Mini - XML library is licensed under the Apache License Version 2.0 with an\n" \
+			"exception to allow linking against GPL2 / LGPL2 - only software.");
 	}
 
 	wShow(aboutW);
