2b8afe536715d2b327ee61429fe0d8886b38d533
Phil Cameron <pecameron1@gmail.com>
2021-12-14 12:29:41 -0500

    Display version
    xtrkcad -V
    Displays version to stdout and exit(0)

--- app/bin/misc.c
+++ app/bin/misc.c
@@ -3037,7 +3037,7 @@ EXPORT wWin_p wMain(int argc, char * argv[]) {
 	opterr = 0;
 	LogSet("dummy",0);
 
-	while ((c = getopt(argc, argv, "vl:d:c:m")) != -1)
+	while ((c = getopt(argc, argv, "vl:d:c:mV")) != -1)
 		switch (c) {
 		case 'c': /* configuration name */
 			/* test for valid filename */
@@ -3081,6 +3081,10 @@ EXPORT wWin_p wMain(int argc, char * argv[]) {
 			NoticeMessage("Missing parameter for %s", _("Ok"), NULL,
 					argv[optind - 1]);
 			exit(1);
+			break;
+		case 'V': // display version
+			printf("Version: %s\n",XTRKCAD_VERSION);
+			exit(0);
 			break;
 		default:
 			abort();
