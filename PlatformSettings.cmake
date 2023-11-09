5fe3f4af93324d9d259d90d374d4a41709cc24dd
Phil Cameron <pecameron1@gmail.com>
2023-11-09 11:02:37 -0500

    Suppress warnings for deprecated GTypeDebugFlags and GTimeVal symbols.

--- PlatformSettings.cmake
+++ PlatformSettings.cmake
@@ -21,6 +21,9 @@ if(UNIX)
         set(XTRKCAD_USE_GETTEXT_DEFAULT ON)
         set(XTRKCAD_USE_BROWSER_DEFAULT ON)
    endif()
+
+    # glib 2.0 deprecated GTypeDebugFlags and GTimeVal, gtk2 has not been updated
+    set(CMAKE_C_FLAGS "-Wno-deprecated-declarations" CACHE STRING "Suppress GTK2 warnings" FORCE)
 endif()
 
 # Set Win64 flag when a 64 bit build is selected
