ad86c2d11d99d0b85c735fe4c0bcc1d877f7c052
Dave Bullis <dave-sf@sillub.com>
2023-11-17 17:17:48 -0500

    Building on ubuntu 20.04, there is a link error that libmxml.a has a dependency on pthread_getspecific@@GLIB... Google says the solution is to add -pthread to the compile and like Linux only

--- PlatformSettings.cmake
+++ PlatformSettings.cmake
@@ -20,6 +20,8 @@ if(UNIX)
     else()
         set(XTRKCAD_USE_GETTEXT_DEFAULT ON)
         set(XTRKCAD_USE_BROWSER_DEFAULT ON)
+        add_compile_options("-pthread")
+        add_link_options("-pthread")
    endif()
 
     # glib 2.0 deprecated GTypeDebugFlags and GTimeVal, gtk2 has not been updated
