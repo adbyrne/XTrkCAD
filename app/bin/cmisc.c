fda570af0976cec6684df5abb7f07b35e639b782
Dave Bullis <dave-sf@sillub.com>
2024-12-31 16:18:21 -0500

    Command dialogs should be cancel-able with Escape key

--- app/bin/cmisc.c
+++ app/bin/cmisc.c
@@ -467,9 +467,12 @@ void DoDescribe(char * title, track_p trk, descData_p data, descUpdate_t update)
 
 	if (describePG.win == NULL) {
 		/* SDB 5.13.2005 */
-		ParamCreateDialog(&describePG, _("Description"), _("Done"), DescribeDone,
-		                  ParamCancel_Undo,
-		                  TRUE, DescribeLayout, F_RECALLPOS,
+		ParamCreateDialog(&describePG, _("Description"),
+				  //_("Done"), DescribeDone,
+				  NULL, NULL,
+		                  ParamCancel_Reset,
+		                  TRUE, DescribeLayout,
+				  F_RECALLPOS|PD_F_ALT_CANCELLABEL,
 		                  DescribeUpdate);
 		describeCmdButtonEnd = wControlBelow((wControl_p)describePG.helpB);
 	}
