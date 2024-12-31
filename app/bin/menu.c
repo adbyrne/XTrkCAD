d90356bdbbb105f6cf14f7c2aa1818562e812197
Dave Bullis <dave-sf@sillub.com>
2024-12-31 15:45:41 -0500

    revert change to CHMOD dirs.  It is un-unnecessary

--- app/bin/menu.c
+++ app/bin/menu.c
@@ -155,7 +155,7 @@ static void ShowAddElevations(void * unused)
 	if (addElevW == NULL)
 		addElevW = ParamCreateDialog(&addElevPG,
 		                             MakeWindowTitle(_("Change Elevations")), _("Change"), DoAddElev,
-		                             ParamCancel_Undo, FALSE, NULL, 0, NULL);
+		                             ParamCancel_Current, FALSE, NULL, 0, NULL);
 	wShow(addElevW);
 }
 
@@ -201,7 +201,7 @@ static void StartRotateDialog(void * funcVP)
 	rotateDialogCallBack_t func = funcVP;
 	if (rotateW == NULL)
 		rotateW = ParamCreateDialog(&rotatePG, MakeWindowTitle(_("Rotate")),
-		                            _("Ok"), RotateEnterOk, ParamCancel_Undo, FALSE, NULL, 0, NULL);
+		                            _("Ok"), RotateEnterOk, ParamCancel_Current, FALSE, NULL, 0, NULL);
 	ParamLoadControls(&rotatePG);
 	rotateDialogCallBack = func;
 	wShow(rotateW);
@@ -224,7 +224,7 @@ static void StartMoveDialog(void * funcVP)
 	moveDialogCallBack_t func = funcVP;
 	if (moveW == NULL)
 		moveW = ParamCreateDialog(&movePG, MakeWindowTitle(_("Move")), _("Ok"),
-		                          MoveEnterOk, ParamCancel_Undo, FALSE, NULL, 0, NULL);
+		                          MoveEnterOk, ParamCancel_Current, FALSE, NULL, 0, NULL);
 	ParamLoadControls(&movePG);
 	moveDialogCallBack = func;
 	wShow(moveW);
