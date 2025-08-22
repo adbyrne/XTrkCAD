db04a66c453ada0e8aeb610f33daa03a43dfede2
Martin Fischer <m_fischer@users.sourceforge.net>
2025-08-22 18:21:14 +0200

    Refactor astyle Close Refactor Map Window branch

--- app/bin/mapwindow.c
+++ app/bin/mapwindow.c
@@ -4,26 +4,26 @@
  * \author Martin Fischer
  */
 
- /*  XTrackCad - Model Railroad CAD
-  *  Copyright (C) 2005, 2025 Dave Bullis
-  *
-  *  This program is free software; you can redistribute it and/or modify
-  *  it under the terms of the GNU General Public License as published by
-  *  the Free Software Foundation; either version 2 of the License, or
-  *  (at your option) any later version.
-  *
-  *  This program is distributed in the hope that it will be useful,
-  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  *  GNU General Public License for more details.
-  *
-  *  You should have received a copy of the GNU General Public License
-  *  along with this program; if not, write to the Free Software
-  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-  */
+/*  XTrackCad - Model Railroad CAD
+ *  Copyright (C) 2005, 2025 Dave Bullis
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 #include <wlib.h>
- 
+
 #include "common.h"
 #include "draw.h"
 #include "fileio.h"
@@ -34,13 +34,19 @@
 
 #include "xtctypes.h"
 
+#define MIN_SCALE 0.01
+#define MAX_SCALE 64.0
+#define MAP_BORDER_PIXELS 2
+#define SCREEN_SIZE_FACTOR 0.5
+
 static int log_mapsize = 0;
 static int log_mapredraw = 0;
 
 static int delayUpdate = 1;
 
 static void DoMapPan(wAction_t action, coOrd pos);
-static wBool_t MapRedraw(wControl_p bd, void* pContex, wWinPix_t px, wWinPix_t py);
+static wBool_t MapRedraw(wControl_p bd, void* pContex, wWinPix_t px,
+                         wWinPix_t py);
 
 EXPORT drawCmd_t mapD = {
 	NULL, &screenDrawFuncs, DC_SIMPLE, INIT_MAP_SCALE, 0.0, {0.0,0.0}, {96.0,48.0}, Pix2CoOrd, CoOrd2Pix, 96.0
@@ -86,20 +92,17 @@ static void DoMapPan(wAction_t action, coOrd pos)
 	case C_DOWN:
 		if (mode == noPan) {
 			mode = movePan;
-		}
-		else {
+		} else {
 			break;
 		}
 	case C_MOVE:
 		if (mode != movePan) {
 			break;
 		}
-		//		mainD.orig.x = pos.x - mainD.size.x/2.0;
-		//		mainD.orig.y = pos.y - mainD.size.y/2.0;
 		panCenter = pos;
 		LOG(log_pan, 1, ("%s = [ %0.3f, %0.3f ]\n", action == C_DOWN ? "START" : "MOVE",
-			mainD.orig.x, mainD.orig.y))
-			PanHere(I2VP(2));
+		                 mainD.orig.x, mainD.orig.y))
+		PanHere(I2VP(2));
 		break;
 	case C_UP:
 		if (mode != movePan) {
@@ -108,14 +111,13 @@ static void DoMapPan(wAction_t action, coOrd pos)
 		panCenter = pos;
 		PanHere(I2VP(0));
 		LOG(log_pan, 1, ("FINAL = [ %0.3f, %0.3f ]\n", mainD.orig.x, mainD.orig.y))
-			mode = noPan;
+		mode = noPan;
 		break;
 
 	case C_RDOWN:
 		if (mode == noPan) {
 			mode = resizePan;
-		}
-		else {
+		} else {
 			break;
 		}
 		mapOrig = pos;
@@ -124,8 +126,8 @@ static void DoMapPan(wAction_t action, coOrd pos)
 		xscale = mainD.scale; //start at current
 		panCenter = pos;
 		LOG(log_pan, 1, ("START %0.3fx%0.3f %0.3f+%0.3f\n", mapOrig.x, mapOrig.y,
-			size.x, size.y))
-			break;
+		                 size.x, size.y))
+		break;
 
 	case C_RMOVE:
 		if (mode != resizePan) {
@@ -151,11 +153,11 @@ static void DoMapPan(wAction_t action, coOrd pos)
 		}
 		xscale = ceil(xscale);
 
-		if (xscale < 0.01) {
-			xscale = 0.01;
+		if (xscale < MIN_SCALE) {
+			xscale = MIN_SCALE;
 		}
-		if (xscale > 64) {
-			xscale = 64;
+		if (xscale > MAX_SCALE) {
+			xscale = MAX_SCALE;
 		}
 
 		mainD.size.x = size.x * xscale;
@@ -165,8 +167,8 @@ static void DoMapPan(wAction_t action, coOrd pos)
 		tempD.scale = mainD.scale = xscale;
 		PanHere(I2VP(2));
 		LOG(log_pan, 1, ("MOVE SCL:%0.3f %0.3fx%0.3f %0.3f+%0.3f\n", xscale,
-			mainD.orig.x, mainD.orig.y, mainD.size.x, mainD.size.y))
-			InfoScale();
+		                 mainD.orig.x, mainD.orig.y, mainD.size.x, mainD.size.y))
+		InfoScale();
 		break;
 
 	case C_RUP:
@@ -189,13 +191,14 @@ static void DoMapPan(wAction_t action, coOrd pos)
 	}
 }
 
- /**
-  * Redraw the Map window using the Scale derived from the Window size and Room size
-  * \param bd [inout] Map canvas - not used
-  * \param pContext [inout] Param context - not used
-  * \param px, py [in] canvas size
-  */
-static wBool_t MapRedraw( wControl_p bd, void* pContex, wWinPix_t px, wWinPix_t py)
+/**
+ * Redraw the Map window using the Scale derived from the Window size and Room size
+ * \param bd [inout] Map canvas - not used
+ * \param pContext [inout] Param context - not used
+ * \param px, py [in] canvas size
+ */
+static wBool_t MapRedraw( wControl_p bd, void* pContex, wWinPix_t px,
+                          wWinPix_t py)
 {
 	if (inPlaybackQuit) {
 		return FALSE;
@@ -208,7 +211,7 @@ static wBool_t MapRedraw( wControl_p bd, void* pContex, wWinPix_t px, wWinPix_t 
 	if (delayUpdate) {
 		wDrawDelayUpdate(mapD.d, TRUE);
 	}
-	//wSetCursor( mapD.d, wCursorWait );
+
 	wBool_t bTemp = wDrawSetTempMode(mapD.d, FALSE);
 	if (bTemp) {
 		printf("MapRedraw TempMode\n");
@@ -231,8 +234,8 @@ static wBool_t MapRedraw( wControl_p bd, void* pContex, wWinPix_t px, wWinPix_t 
 	// Find new mapD.scale
 	if ((px <= 0 || py <= 0) && mapD.d) {
 		wDrawGetSize(mapD.d, &px, &py);
-		px += 2;
-		py += 2;
+		px += MAP_BORDER_PIXELS;
+		py += MAP_BORDER_PIXELS;
 	}
 	if (px > 0 && py > 0) {
 		FLOAT_T scaleX = mapD.size.x * mapD.dpi / px;
@@ -240,25 +243,26 @@ static wBool_t MapRedraw( wControl_p bd, void* pContex, wWinPix_t px, wWinPix_t 
 		FLOAT_T scale;
 
 		// Find largest scale
-		if (scaleX > scaleY) { scale = scaleX; }
-		else { scale = scaleY; }
+		if (scaleX > scaleY)
+		{ scale = scaleX; }
+		else
+		{ scale = scaleY; }
 
 		if (scale > MAX_MAIN_SCALE) { scale = MAX_MAIN_SCALE; }
 		if (scale < MIN_MAIN_MACRO) { scale = MIN_MAIN_MACRO; }
 
 		scale = ceil(scale);	// Round up
 		LOG(log_mapsize, 2,
-			("      %ldx%ld mapD.scale=%0.3f, scaleX=%0.3f scaleY=%0.3f scale=%0.3f\n",
-				px, py, mapD.scale, scaleX, scaleY, scale));
+		    ("      %ldx%ld mapD.scale=%0.3f, scaleX=%0.3f scaleY=%0.3f scale=%0.3f\n",
+		     px, py, mapD.scale, scaleX, scaleY, scale));
 		mapD.scale = scale;
-	}
-	else {
+	} else {
 		LOG(log_mapsize, 2, ("  0x0 mapD.scale=%0.3f\n", mapD.scale));
 	}
 	wDrawClear(mapD.d);
 	DrawTracks(&mapD, mapD.scale, mapD.orig, mapD.size);
 	MapDrawBoundingBox(TRUE);
-	//wSetCursor( mapD.d, defaultCursor );
+
 	wDrawSetTempMode(mapD.d, bTemp);
 	wDrawDelayUpdate(mapD.d, FALSE);
 
@@ -279,25 +283,24 @@ void MapChangeScale()
 	wWinPix_t w, h;
 	FLOAT_T fw, fh;
 
-	// Restrict map size to 1/2 of screen
-	FLOAT_T fScaleW = mapD.size.x / (displayWidth * 0.5 / mapD.dpi);
-	FLOAT_T fScaleH = mapD.size.y / (displayHeight * 0.5 / mapD.dpi);
+	// Restrict map size to SCREEN_SIZE_FACTOR of screen
+	FLOAT_T fScaleW = mapD.size.x / (displayWidth * SCREEN_SIZE_FACTOR / mapD.dpi);
+	FLOAT_T fScaleH = mapD.size.y / (displayHeight * SCREEN_SIZE_FACTOR / mapD.dpi);
 	FLOAT_T fScale = ceil(max(fScaleW, fScaleH));
 	if (fScale > mapD.scale) {
 		LOG(log_mapsize, 2, ("  ChangeMapScale incr scale from %0.3f to %0.3f\n",
-			mapD.scale, fScale));
+		                     mapD.scale, fScale));
 		mapD.scale = fScale;
 	}
 
-	fw = (((mapD.size.x / mapD.scale) * mapD.dpi) + 0.5) + 2;
-	fh = (((mapD.size.y / mapD.scale) * mapD.dpi) + 0.5) + 2;
+	fw = (((mapD.size.x / mapD.scale) * mapD.dpi) + 0.5) + MAP_BORDER_PIXELS;
+	fh = (((mapD.size.y / mapD.scale) * mapD.dpi) + 0.5) + MAP_BORDER_PIXELS;
 
 	w = (wWinPix_t)fw;
 	h = (wWinPix_t)fh;
 	LOG(log_mapsize, 2, ("  ChangeMapScale mapD.scale=%0.3f w=%ld h=%ld\n",
-		mapD.scale, w, h));
-	// wWinSetSize(mapW, w + mapBorderW, h + mapBorderH);
-	// This should be done by wWinSetSize
+	                     mapD.scale, w, h));
+
 	wDrawSetSize(mapD.d, w, h, NULL);
 	MapRedraw(mapD.d, NULL, 0, 0);
 }
@@ -305,13 +308,13 @@ void MapChangeScale()
 wBool_t MapGetVisiblePref(void)
 {
 
-// visibility toggle for map window
-// get the start value
-long mapVisible_long;
-wPrefGetInteger("map", "visible", &mapVisible_long, 1);
-mapVisible = mapVisible_long ? TRUE : FALSE;
+	// visibility toggle for map window
+	// get the start value
+	long mapVisible_long;
+	wPrefGetInteger("map", "visible", &mapVisible_long, 1);
+	mapVisible = mapVisible_long ? TRUE : FALSE;
 
-return mapVisible;
+	return mapVisible;
 }
 
 /**
@@ -353,19 +356,19 @@ EXPORT void MapWindowShow(int state)
 	ToggleSetInMenuToolbar(mapShowMI, mapShowB, mapVisible);
 }
 
-wControl_p 
+wControl_p
 MapWindowCreate()
 {
-	/*w = (wWinPix_t)((mapD.size.x/mapD.scale)*mainD.dpi + 0.5)+2;*/
-	/*h = (wWinPix_t)((mapD.size.y/mapD.scale)*mainD.dpi + 0.5)+2;*/
-	//FormRegister(&mapPG);
+
 	LOG(log_mapsize, 2, ("DrawInit/FormCreateDialog(&mapPG\n"));
 
 	mapW = FormCreateDialog(&mapPG, _("Map"),
-		NULL, NULL,
-		NULL, NULL,
-		FALSE, F_RESIZE, NULL);
+	                        NULL, NULL,
+	                        NULL, NULL,
+	                        FALSE, F_RESIZE, NULL);
 	mapD.d = MAPCANVASCONTROL;
+
+	/** \TODO Uncomment and enable ChangeMapScale() */
 	//ChangeMapScale();
 
 	return(mapW);
